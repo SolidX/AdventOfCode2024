496f884060d7e1abae67195db478b33c15c44e8f
SolidX <SolidX@users.noreply.github.com>
2024-12-03 19:52:55 -0500

    Day 3 Part 2

--- Day 03/Program.cs
+++ Day 03/Program.cs
@@ -4,6 +4,8 @@ namespace Day_03
 {
     internal class Program
     {
+        private static Regex mulInstrRegex = new Regex("mul\\((?<Operand1>\\d{1,3}),(?<Operand2>\\d{1,3})\\)", RegexOptions.Compiled);
+
         static void Main(string[] args)
         {
             WriteHeader(3);
@@ -11,6 +13,7 @@ namespace Day_03
             var rawInput = LoadInput();
             
             Part1(rawInput);
+            Part2(rawInput);
         }
 
         static void WriteHeader(int day)
@@ -34,18 +37,53 @@ namespace Day_03
 
         static void Part1(string[] rawInput)
         {
-            var instructionRegex = new Regex("mul\\((\\d{1,3}),(\\d{1,3})\\)", RegexOptions.Compiled);
             Console.WriteLine("~ Part 1 ~");
             Console.WriteLine();
 
             var totalMultiplications = 0l;
             foreach (var line in rawInput)
             {
-                var matches = instructionRegex.Matches(line);
+                var matches = mulInstrRegex.Matches(line);
                 foreach (Match m in matches)
                 {
                     totalMultiplications += Int32.Parse(m.Groups[1].Value) * Int32.Parse(m.Groups[2].Value);
                 }
+            }
+
+            Console.WriteLine($"Total Multiplications: {totalMultiplications}");
+            Console.WriteLine();
+        }
+
+        static void Part2(string[] rawInput)
+        {
+            var processingRegex = new Regex("(do|don't)\\(\\)", RegexOptions.Compiled);
+            Console.WriteLine("~ Part 2 ~");
+            Console.WriteLine();
+
+            var directive = true;
+            var totalMultiplications = 0l;
+            var processingDirectives = new Dictionary<int, bool>() { { 0, true } };
+
+            foreach (var line in rawInput)
+            {
+                var pMatches = processingRegex.Matches(line);
+                foreach (Match m in pMatches)
+                    processingDirectives.Add(m.Index, m.Value.Equals("do()", StringComparison.OrdinalIgnoreCase));
+
+                var iMatches = mulInstrRegex.Matches(line);                
+                foreach (Match m in iMatches)
+                {                    
+                    var op1 = Int32.Parse(m.Groups["Operand1"].Value);
+                    var op2 = Int32.Parse(m.Groups["Operand2"].Value);
+
+                    var mostRecentDirective = processingDirectives.Keys.Where(idx => idx <= m.Index).Max();
+                    directive = processingDirectives[mostRecentDirective];
+                    if (directive)
+                        totalMultiplications += op1 * op2;
+                }
+
+                processingDirectives.Clear();
+                processingDirectives.Add(0, directive);
             }
 
             Console.WriteLine($"Total Multiplications: {totalMultiplications}");
