c079a42f780775f9c4c110af4cd65bfa2993d55d
SolidX <SolidX@users.noreply.github.com>
2024-12-09 21:03:40 -0500

    Day 4 Part 2

--- Day 04/Program.cs
+++ Day 04/Program.cs
@@ -8,6 +8,7 @@
 
             var puzzle = LoadInput(); //.ToCharArray();
             Part1(puzzle);
+            Part2(puzzle);
         }
 
         static void WriteHeader(int day)
@@ -39,6 +40,22 @@
                 for (var x = Math.Max(0, col - 1); x <= Math.Min(col + 1, maxCol); x++)
                 {
                     if (y == row && x == col)
+                        continue;
+
+                    yield return Tuple.Create(x, y);
+                }
+            }
+        }
+        static IEnumerable<Tuple<int, int>> GetDiagonalCoordinates(int row, int col, string[] grid)
+        {
+            var maxRow = grid.Length - 1;
+            var maxCol = grid[row].Length - 1;
+
+            for (var y = Math.Max(0, row - 1); y <= Math.Min(row + 1, maxRow); y++)
+            {
+                for (var x = Math.Max(0, col - 1); x <= Math.Min(col + 1, maxCol); x++)
+                {
+                    if (y == row || x == col)
                         continue;
 
                     yield return Tuple.Create(x, y);
@@ -89,6 +106,41 @@
             }
 
             Console.WriteLine($"'XMAS'-es Found: {xmasCount}");
+            Console.WriteLine();
+        }
+
+        static void Part2(string[] puzzle)
+        {
+            Console.WriteLine("~ Part 2 ~");
+            Console.WriteLine();
+
+            var maxY = puzzle.Length - 1;
+            var maxX = puzzle[0].Length - 1;
+            var xmasCount = 0;
+
+            for (var y = 0; y <= maxY; y++)
+            {
+                for (var x = 0; x <= maxX; x++)
+                {
+                    if (puzzle[y][x] != 'A')
+                        continue;
+
+                    var neighbors = GetDiagonalCoordinates(y, x, puzzle).ToList();
+
+                    if (neighbors.Count != 4 || !neighbors.All(coords => puzzle[coords.Item2][coords.Item1] == 'M' || puzzle[coords.Item2][coords.Item1] == 'S'))
+                        continue;
+
+                    var n0 = puzzle[neighbors[0].Item2][neighbors[0].Item1];
+                    var n1 = puzzle[neighbors[1].Item2][neighbors[1].Item1];
+                    var n2 = puzzle[neighbors[2].Item2][neighbors[2].Item1];
+                    var n3 = puzzle[neighbors[3].Item2][neighbors[3].Item1];
+
+                    if (n0 != n3 && n1 != n2) //Ensure the opposite ends of the X don't match
+                        xmasCount++;
+                }
+            }
+
+            Console.WriteLine($"'X-MAS'-es Found: {xmasCount}");
             Console.WriteLine();
         }
     }
