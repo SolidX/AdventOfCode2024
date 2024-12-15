dba21d081c137e87e9ed596c81ee7c9db1dd7e56
SolidX <SolidX@users.noreply.github.com>
2024-12-15 14:12:50 -0500

    Day 7 Part 1

---
+++ Day 07/Equation.cs
@@ -0,0 +1,104 @@
+﻿namespace Day_07
+{
+    internal class Equation
+    {
+        public long TestValue {get; set;}
+        public List<int> Operands { get; private set; }
+        private List<char> Operators { get; set; }
+
+        public Equation(long tVal, IEnumerable<int> operands)
+        {
+            TestValue = tVal;
+            Operands = operands.ToList();
+            Operators = new List<char>(Operands.Count - 1);
+        }
+
+        public bool Passes()
+        {
+            if (Operators.Any())
+                return TestValue == Evaluate();
+
+            var possiblities = new List<Equation>();
+            
+            var p = Clone();
+            p.Operators.Add('+');
+            possiblities.Add(p);
+
+            var q = Clone();
+            q.Operators.Add('*');
+            possiblities.Add(q);
+
+            for (var i = 1; i < Operands.Count - 1; i++)
+            {
+                var tmp = new List<Equation>();
+
+                for (int j = 0; j < possiblities.Count; j++)
+                {
+                    var a = possiblities[j].Clone();
+                    a.Operators.Add('+');
+                    tmp.Add(a);
+                    var b = possiblities[j].Clone();
+                    b.Operators.Add('*');
+                    tmp.Add(b);
+                }
+
+                possiblities = tmp;
+            }
+
+            return possiblities.Any(x => x.TestValue == x.Evaluate());
+            //return outcomes.Any(x => x == TestValue);
+        }
+
+        public long? Evaluate()
+        {
+            if (!Operators.Any() || !Operands.Any())
+                return null;
+
+            var result = (long)Operands.First();
+            for (var i = 0; i < Operators.Count; i++)
+            {
+                switch (Operators[i])
+                {
+                    case '+':
+                        result += Operands[i + 1];
+                        break;
+                    case '*':
+                        result *= Operands[i + 1];
+                        break;
+                    default:
+                        throw new ArgumentException($"Invalid operator: '{Operators[i]}'");
+                }
+            }
+
+            return result;
+        }
+
+        public override string ToString()
+        {
+            if (Operators.Count == 0)
+                return String.Join('?', Operands);
+
+            if (Operands.Count() == 0)
+                return string.Empty;
+
+            var str = Operands.First().ToString();
+            for (int i = 0; i < Operators.Count; i++)
+                str += $"{Operators[i]}{Operands[i + 1]}";
+            return str;
+        }
+
+        public Equation Clone()
+        {
+            var operands = new List<int>(Operands.Count);
+            operands.AddRange(Operands);
+
+            var operators = new List<char>(Operators.Count);
+            operators.AddRange(Operators);
+
+            var eq = new Equation(TestValue, operands);
+            eq.Operators = operators;
+
+            return eq;
+        }
+    }
+}
---
+++ Day 07/Program.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections.Concurrent;
+
+namespace Day_07
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteHeader(7);
+
+            var rawInput = LoadInput();
+            var parsedInput = ParseInput(rawInput);
+            Part1(parsedInput);
+        }
+
+        static void WriteHeader(int day)
+        {
+            Console.WriteLine("+==========================+");
+            Console.WriteLine("| Advent of Code -- Day {0:D2} |", day);
+            Console.WriteLine("+==========================+");
+        }
+
+        static string[] LoadInput()
+        {
+            if (File.Exists("input.txt"))
+            {
+                return File.ReadAllLines("input.txt");
+            }
+            else
+            {
+                return [];
+            }
+        }
+
+        static List<Equation> ParseInput(string[] rawInput)
+        {
+            var equations = new ConcurrentBag<Equation>();
+            Parallel.ForEach(rawInput, (input, i) =>
+            {
+                var vals = input.Split(':', 2, StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries);
+                var testVal = Int64.Parse(vals[0]);
+                var operands = vals[1].Split(' ', StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries).Select(Int32.Parse);
+                equations.Add(new Equation(testVal, operands));
+            });
+            return equations.ToList();
+        }
+
+        static void Part1(List<Equation> calibrations)
+        {
+            Console.WriteLine("~ Part 1 ~");
+            Console.WriteLine();
+
+            var total = calibrations.Where(x => x.Passes()).Sum(x => x.TestValue);
+
+            Console.WriteLine($"Total Calibration Result: {total}");
+            Console.WriteLine();
+        }
+    }
+}
